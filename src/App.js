acc46d234532bc1b0337d9579ccfd233cc656290
Dev-LucasC <lucaconceicao@hotmail.com>
2023-06-10 16:41:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
+import abc from "./abcdovoleibol.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="border-container">
+        <div className="container">
+          <header className="App-header">
+            <div className="intro">
+              <span className="small">oi,👋 eu sou o Lucas</span>
+            </div>
+            <div className="fullstack">
+              <div className="fullstack-text">
+                <p>FullStack</p>
+                <div className="icons">
+                  <a
+                    href="https://www.linkedin.com/seu-linkedin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLinkedin className="icon" />
+                  </a>
+                  <a
+                    href="https://github.com/seu-usuario"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub className="icon" />
+                  </a>
+                  <a href="seu-cv.pdf" download>
+                    <FiDownload className="icon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <h2 className="desenvolvedor">Desenvolvedor</h2>
+            <p className="intro-text">
+              Tenho 20 anos com 2 anos de 
+              <br></br>experiência trabalhando 
+             
+              em projetos 
+               em
+              produção.
+            </p>
+          </header>
+        </div>
+      </div>
+      <section>
+        <h2>Meus Projetos</h2>
+        <div className="projects-section">
+          <div className="project">
+            <img src={abc} alt="Projeto 1" />
+            <h3>Título do Projeto 1</h3>
+            <p>Breve explicação do Projeto 1.</p>
+          </div>
+          <div className="project">
+            <img src="projeto-2.png" alt="Projeto 2" />
+            <h3>Título do Projeto 2</h3>
+            <p>Breve explicação do Projeto 2.</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
